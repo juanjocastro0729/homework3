1ef80a6121311aece24c52c63125f6285d7b29c0
Juanjo Castro Rodriguez <juanjocastro0729@gmail.com>
2025-09-12 23:37:35 -0500

    first commit of all the files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import {ref} from 'vue'
+
+    const name = ref('')
+</script>
+
+<template>
+
+    <main class="app">
+
+        <section class="greeting"> 
+            <h2 class="title">
+                Welcome back, <input type="text" placeholder="Enter Name" v-model="name"></input>
+            </h2>
+        </section>
+
+        <section class="create-todo">
+
+        </section>
+
+        <section class="todo-list">
+
+        </section>
+
+    </main>
+
+</template>
